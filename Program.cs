8d486cb4d474295967693bbf4636f6330c21b24a
Yuki <yuki08281993@gmail.com>
2018-08-11 00:32:02 +0900

    first commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using static System.Console;
+using static Zdk;
+
+static class Program {
+    static Random random = new Random();
+    static void Main() => Infinite().Select(_ => AtRandom(Zun, Doko)).Judge().ForEach(WriteLine);
+    static IEnumerable<int> Infinite() { while (true) yield return 0; }
+    static T AtRandom<T>(params T[] args) => args[random.Next(args.Length)];
+
+    static IEnumerable<object> Judge(this IEnumerable<Zdk> source) {
+        var history = new Queue<Zdk>();
+
+        foreach (var zdk in source) {
+            yield return zdk;
+            history.SizedEnqueue(zdk, 5);
+            Zdk[] pattern = { Zun, Zun, Zun, Zun, Doko };
+            if (history.SequenceEqual(pattern)) break;
+        }
+        yield return Kiyoshi;
+    }
+}
+
+enum Zdk { Zun, Doko, Kiyoshi }
+
+static class QueueExtention {
+    public static void SizedEnqueue<T>(this Queue<T> queue, T item, int size) {
+        queue.Enqueue(item);
+        if (queue.Count > size) queue.Dequeue();
+    }
+}
